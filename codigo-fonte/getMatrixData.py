b0584710750e9cb6912d5f2468e79066d9d18144
Matheus Vassoler <matheus.s.vassoler@gmail.com>
2018-06-14 15:19:54 -0300

    Adiciona comentarios no arquivo getMatrixData

--- codigo-fonte/getMatrixData.py
+++ codigo-fonte/getMatrixData.py
@@ -14,25 +14,32 @@ def getMatrixData(city, state, sheet_name):
     path2 = "./SJC_RMRJ_data/"+city.upper()+"_population.xlsx"
     dfPopulation = pd.read_excel(path2)
 
-    #get total of regions into the matrix
+    #get total of regions into the matrix. It's necessary to interate over the matrix that contais the populations of each zone
     nr = dfPopulation.index.values.size
 
     #print(dfDistanceTime)
     #print(dfPopulation)
     #print(path1)
 
+    #Set i(origin) and j(destination) with 0, because in the matrix, row and column start with 0
     i = 0
     j = 0
+
+    #We need three lists, one to store pi(quantity of people in zone i), other to store pj(quantity of people in zone j)
+    #And finally the third list to store dij(distance between i and j)
     pi = []
     pj = []
     dij = []
 
     while i < nr:
        while j < nr:
+           #Verify if i is different of j, because is impossible get data between equal zone (example, distane between i and i)
            if i != j:
-               #print('{} {}'.format(i, j))
+               #Using iloc, the data is obtained from dataframe and is added in the list
                pi.append(dfPopulation.iloc[i][1])
                pj.append(dfPopulation.iloc[j][1])
+               #In matrix, exist failed mappings, so it's necessary to treat the data, replace X for NaN.
+               #This is did using numpy library
                if(dfDistanceTime.iloc[i][j] == 'X'):
                    dfDistanceTime.replace('X', np.NaN)
                else:
