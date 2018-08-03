31f949aa844b4c45b30c1d6b5868084709026686
Matheus Vassoler <matheus.s.vassoler@gmail.com>
2018-08-02 23:51:02 -0300

    Altera o arquivo gravitationalModel

--- codigo-fonte/gravitationalModel.py
+++ codigo-fonte/gravitationalModel.py
@@ -8,23 +8,44 @@ def func(X, K, s):
     dij = X[2]
     return (K*pi*pj)/(pow(dij,s))
 
-def calculation():
+def calculation(resultQuantity):
     #Call the function that contains informations about pi, pj and dij
     #In this case we want the informations about the Sao Jose dos Campos city
     #We want only distance (dij), the matrix has distance and time, so it's passed an argument that specific the getting of the distances
-    matrix = getMatrixData('rmrj', 'rj', 'DrivingDistance')
+    matrix = getMatrixData('sjc', 'sp', 'DrivingDistance', 'Driving')
 
     pi = np.asarray(matrix[0])
     pj = np.asarray(matrix[1])
     dij = np.asarray(matrix[2])
+    F = np.asarray(matrix[3])
 
-    F = func([pi,pj,dij], 2.2, 1.5)
+    for idx,val in enumerate(pi):
+        print("[%d] pi: %d -- pj: %d -- dij: %d -- F: %d" %(idx,pi[idx],pj[idx], dij[idx], F[idx]))
 
-    F_noise = 0.2 * np.random.normal(size=pi.size)
-    F = F + F_noise
-    np.random.seed(179)
+    KEstimation = np.random.uniform(10e-7, 10e-2, resultQuantity)
+    sEstimation = np.random.uniform(0.1, 0.4, resultQuantity)
 
-    K, s = curve_fit(func, [pi, pj, dij], F)
-    print(s)
+    #Variable that contains all the values of the K got by curve_fit
+    KList = []
 
-calculation()
+    #Variable that contains all the values of the s got by curve fit
+    sList = []
+
+
+    for i in range(resultQuantity):
+        p0 = [KEstimation[i], sEstimation[i]]
+
+        K,s = curve_fit(func, [pi, pj, dij], F, p0=p0)
+
+        KList.append(K.tolist())
+        sList.append(s.tolist())
+
+
+    print(KList)
+    print(sList)
+    print(len(KList))
+    print(len(sList))
+    print(KEstimation)
+    print(sEstimation)
+
+calculation(1000)
