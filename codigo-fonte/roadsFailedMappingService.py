84dd710e109feafd4d9dfec6e53d2857081ff8d6
Matheus Vassoler <matheus.s.vassoler@gmail.com>
2018-08-03 00:38:24 -0300

    Altera o arquivo roadsFailedMappingService

--- codigo-fonte/roadsFailedMappingService.py
+++ codigo-fonte/roadsFailedMappingService.py
@@ -10,10 +10,24 @@ def roadsFailedMappingService(inputFile, city, state):
     con = databaseConnection.connectdb()
     if not con:
         print('Database connection ERROR!')
-        exit();
+        exit()
     cursor = con.cursor()
 
+    # list that contains the google's api key (you can change it)
+    apikey = ['AIzaSyAr11wo7Q90rmf7ZReZFVz3OV50g5iW1hI', 'AIzaSyB-kB6JZ-UkSFVGXX0eEIavyMGg-w2whnk',
+              'AIzaSyClzkVxwXbjphXktb1LZ_MJCprlJMEDvqw', 'AIzaSyDl3vAN7saDAubpUh2f4xb1wIWOhG_lxVc',
+              'AIzaSyBSFnV2oNW-fR8yiMBzxZkTFKpyOLRM4-8', 'AIzaSyBg9nMa0mAAnZyoyCsCO7l7mDS3fciI32c',
+              'AIzaSyDZke-btxWiEtGsNisNBicP5zsJhiG4XS0', 'AIzaSyD79s5kzm_4WJGJKnp6sv2lAADpGeGZEZ4',
+              'AIzaSyDd1KnOEb6u79sJgT4cVV1JrSOcKkvFtBI', 'AIzaSyDwsqAZ-8wB_h23XhKAVshX0MZqa_ty4GI',
+              'AIzaSyAz1ZrldhSNHUdXjvT9MTDUeiub4MeUND8', 'AIzaSyBQYl-sChyjHoc186BYpKrId6-9nf2Kf9A',
+              'AIzaSyDAmvH9hkaw5NwDHt1_3VPuNX6KFqtcZTo', 'AIzaSyBUguv05oZBYjCZ7OJtxYlUUyzHoyOqlPU',
+              'AIzaSyACauD8ibd4RDDCb_1C57xm2iJG4Wmj3c8', 'AIzaSyCIdkrYIOqYX3cogj7LnwAi7Kogk2pDcQc',
+              'AIzaSyB4mwWf6m898vPQYd0lGG4-5NDQt0myS4o', 'AIzaSyDBRAo1aS3ihZbj9SnwWD07kA7q1B9XpLA',
+              'AIzaSyAYoIztpjHUTGGuEKD25_1fnwsIdNEm3FA', 'AIzaSyAC8xAfiMw66cZTNqrp0WA8Y9h2hxGNAHg']
 
+    # variable that control the use of the keys (you can change it)
+    #The apiKey list starts with the keyUse value
+    keyUse = 0
 
     if inputFile == "zonas-trafego-sjc-sp":
         # Read from a XLS a matrix where each line is a pair of coords lat/long
@@ -23,13 +37,10 @@ def roadsFailedMappingService(inputFile, city, state):
         regions = importGeographicalPointsFromTXT.importGeographicalPointsFromTXT(inputFile)
 
     # Variable that contains the sheet name of the matrix (you can change it)
-    sheetName = "WalkingDistance"
+    sheetName = "DrivingDistance"
 
     #Variable that contains the model of travel, it is used by the API of Google Maps (you can change it)
-    tempmode = "walking"
-
-    #Key to use the API of Google Maps (you can change it)
-    key = "AIzaSyB-kB6JZ-UkSFVGXX0eEIavyMGg-w2whnk"
+    tempmode = "driving"
 
     #Get a list that contains the tuples with de origin and destination pair of the failed mappings
     failedMappingList = getFailedMappings.getFailedMappings(city, state, sheetName)
@@ -41,26 +52,11 @@ def roadsFailedMappingService(inputFile, city, state):
 
     j = 0
 
-    '''while j < len(regions):
-        temp = regions[j]
-        origins = str(temp[1]) + "," + str(temp[0])
-        print(origins)
-        j += 1'''
-
     cc = 1
-
     while i < listLen:
         # Setup initial index of origin and destination using the first tuple that inside the failedMappingList
         idx_origin = failedMappingList[i][0]
         idx_destination = failedMappingList[i][1]
-
-        '''if idx_origin > idx_destination:
-            aux = idx_origin
-            idx_origin = idx_destination
-            idx_destination = aux
-            flag = True
-        else:
-            flag = False'''
 
         temp = regions[idx_origin]
         origins = str(temp[1]) + "," + str(temp[0])
@@ -71,7 +67,7 @@ def roadsFailedMappingService(inputFile, city, state):
 
         try:
             # Get the distance for forward path
-            [distance, duration, paths] = getDirectionTwoGeographicalPoints.getDirectionTwoGeographicalPoints(origins, destinations, tempmode, None, apikey[keyUse])
+            [distance, duration, paths] = getDirectionTwoGeographicalPoints.getDirectionTwoGeographicalPoints(origins, destinations, tempmode.upper(), None, apikey[keyUse])
             #print(idx_origin, " - ", idx_destination)
             # Record distance and path into database
             output = recordDistancesDirections.recordDistancesDirections(idx_origin, idx_destination, origins, destinations, tempmode, distance, paths, city, state, duration, 0)
@@ -79,12 +75,14 @@ def roadsFailedMappingService(inputFile, city, state):
             #Record distance and path into database
             output = recordDistancesDirections.recordDistancesDirections(idx_origin, idx_destination, origins, destinations, tempmode, '-1', [], city, state, '-1', 1)
 
+        #The daily quota of the API is 2500, but to avoid failures, only 1900 requests per key
         if cc < 1900:
             cc += 1
         else:
-            if keyUse == 19:
+            #You can choose this number
+            if keyUse == 13:
                 cc == cc
-                break;
+                break
             else:
                 keyUse += 1
                 cc = 1
